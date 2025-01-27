084ea474a3759b4268d6d8d0f6f7328cfcce5944
glitched-sudhanshu <r02.sudhanshu@gmail.com>
2025-01-27 12:44:00 +0530

    [feat][wip] add data store helper class

---
+++ app/src/main/java/com/myjar/jarassignment/data/DataStoreHelper.kt
@@ -0,0 +1,30 @@
+package com.myjar.jarassignment.data
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.myjar.jarassignment.data.utils.AppConstants
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+
+private val Context.dataStore by lazy { preferencesDataStore(AppConstants.DATA_STORE_KEY) }.value
+
+object DataStoreHelper {
+
+    suspend fun save(context: Context, value: String) {
+        val prefKey = stringPreferencesKey(AppConstants.COMPUTER_ITEMS)
+        context.dataStore.edit {
+            it[prefKey] = value
+        }
+    }
+
+
+    fun read(context: Context, key: String): Flow<String?> {
+        val prefKey = stringPreferencesKey(key)
+        return context.dataStore.data.map {
+            it[prefKey]
+        }
+    }
+}
---
+++ app/src/main/java/com/myjar/jarassignment/data/utils/AppConstants.kt
@@ -0,0 +1,6 @@
+package com.myjar.jarassignment.data.utils
+
+object AppConstants {
+    const val DATA_STORE_KEY = "jar_assignment"
+    const val COMPUTER_ITEMS = "compute_items"
+}
